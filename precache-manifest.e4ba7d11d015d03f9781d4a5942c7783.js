6f657ee95d80b281661050b5eb144c43720e134e
Dhrunit <jimmyprajapati99@gmail.com>
2020-06-05 08:23:36 +0530

    Updates

---
+++ precache-manifest.e4ba7d11d015d03f9781d4a5942c7783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2950255df63766f3d150583d5466f937",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89a137848d5ecbf7bea1",
+    "url": "/monsters-rolodex/static/css/main.e26cfeb3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a137848d5ecbf7bea1",
+    "url": "/monsters-rolodex/static/js/main.be70a888.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
